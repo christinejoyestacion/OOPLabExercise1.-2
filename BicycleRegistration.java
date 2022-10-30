1ddf28467774203db360b4bb60f68db231fb74c4
christinejoyestacion <116147896+christinejoyestacion@users.noreply.github.com>
2022-10-30 22:44:34 +0800

    Add files via upload

---
+++ BicycleRegistration.java
@@ -0,0 +1,34 @@
+public class BicycleRegistration {
+	public static void main(String[] args) {
+		Bicycle bike1,bike2,bike3,tagno1,tagno2;	
+		String owner1,owner2,owner3, Tagnum1,Tagnum2,Tagnum3;	
+		
+		bike1= new Bicycle();
+		bike1.setOwnerName("Christine Estacion");
+		
+		bike2= new Bicycle();
+		bike2.setOwnerName("Jennie Kim");
+		
+		owner1=bike1.getOwnerName();
+		owner2=bike2.getOwnerName();
+		
+		tagno1= new Bicycle();
+		tagno1.setTagNo("2003-345L");
+		
+		tagno2= new Bicycle();
+		tagno2.setTagNo("1996-116J");
+		
+		Tagnum1=tagno1.getTagNo();
+		Tagnum2=tagno2.getTagNo();
+		
+		bike3= new Bicycle("1994-114J","Kim Jongin");
+		owner3=bike3.getOwnerName();
+		Tagnum3=bike3.getId();
+		
+		System.out.println("LAB EXERCISE 1.2 OUTPUT\n");
+		System.out.println("This is the output of LabExercise1.1");
+		System.out.println("\t"+owner1+" owns a Bicycle with a tag number: "+Tagnum1);
+		System.out.println("\t"+owner2+" also owns a  Bicycle with a tag number: "+Tagnum2);
+		System.out.println("\nThis is the final output of LabExercise1.2\n\t"+owner3+" owns a Bicycle with a tag number: "+Tagnum3);
+	}
+}
